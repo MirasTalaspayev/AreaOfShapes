2e3c1ed15c4cfcc4c0a4c96d155152656e9cc931
Miras Talaspayev <tmk01.12.00@gmail.com>
2021-08-12 18:26:00 +0600

    Added Exception in contructor for invalid data.

--- AreaOfShapes/Triangle.cs
+++ AreaOfShapes/Triangle.cs
@@ -9,6 +9,8 @@ namespace AreaOfShapes
         public double c { get; }
         public Triangle(double a, double b, double c)
         {
+            if (a + b <= c || a + c <= b || b + c <= a)
+                throw new Exception("Parameters are not valid. Sum of two sides should be less than third one.");
             this.a = a;
             this.b = b;
             this.c = c;
